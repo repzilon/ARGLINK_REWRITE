237badd0ac9c1a10b632423f2539450447905532
repzilon <6383002+repzilon@users.noreply.github.com>
2025-10-02 20:13:37 -0400

    Change assembly attributes
    The hell with Microsoft

--- ARGLINK_REWRITE/Properties/AssemblyInfo.cs
+++ ARGLINK_REWRITE/Properties/AssemblyInfo.cs
@@ -1,36 +1,45 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+#region General Information
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+#endregion
 [assembly: AssemblyTitle("ARGLINK_REWRITE")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyDescription("Argonaut SNES Linker Rewrite")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Exploratorium")]
 [assembly: AssemblyProduct("ARGLINK_REWRITE")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © LuigiBlood 2017, Repzilon 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+#region Interoperability Attributes
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+#endregion
+[assembly: ComVisible(false)]
 [assembly: Guid("e93cc78d-682b-4853-8663-9c2418627747")]
 
+#region Version Information
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+#endregion
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
